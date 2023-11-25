68e797cbe765f5a4424c821fe63ce9a37f060be9
seyeon040768 <seyeon040768@gmail.com>
2023-11-25 23:55:53 +0900

    update(code): Section1 Lecture2

---
+++ code/Section1/Lecture2/Lecture2.cpp
@@ -0,0 +1,16 @@
+﻿/*
+* This code is written by Seyeon Lee.
+*/
+
+#include <iostream>
+
+int main(void)
+{
+	int x = 2;
+	// x = 5;
+	int y = x + 3;
+
+	std::cout << y << std::endl;
+
+	return 0;
+}
