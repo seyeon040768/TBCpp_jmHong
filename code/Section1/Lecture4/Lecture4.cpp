54095e796647d840a1cb44ebe04d28f62a21b4fc
seyeon040768 <seyeon040768@gmail.com>
2023-11-26 19:13:05 +0900

    update(code): Section1 Lecture4

---
+++ code/Section1/Lecture4/Lecture4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdio>
+
+int main(void)
+{
+	using namespace std;
+	int x = 1024;
+	double pi = 3.141592;
+
+	cout << "Hello, World!\n"; // << std::endl;
+	cout << "x is " << x << " pi is " << pi << endl;
+
+	cout << "abc" << "\t" << "def" << endl;
+	cout << "ab" << "\t" << "cdef" << endl;
+
+
+	cout << "Before your input is " << x << endl;
+
+	cin >> x;
+
+	cout << "Your input is " << x << endl;
+
+	return 0;
+}
