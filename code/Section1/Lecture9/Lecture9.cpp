c74805bd36e53eb4b18532d87645981f6c33bf0d
Seyeon Lee <seyeon040768@gmail.com>
2023-11-27 22:47:58 +0900

    update(code): Section1 Lecture9

---
+++ code/Section1/Lecture9/Lecture9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int Add(int x, int y) { return x + y; }
+
+int main(void)
+{
+	cout << "Hello, World" << "abcdef " <<
+		"Hello Home" << endl;
+
+	return 0;
+}
