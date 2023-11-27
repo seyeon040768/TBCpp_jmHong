73a7a4dd9fa5f068b8199ec1ad571a45b4c8f332
Seyeon Lee <seyeon040768@gmail.com>
2023-11-27 22:10:22 +0900

    update(code): Section1 Lecture8

---
+++ code/Section1/Lecture8/Lecture8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int x = 2; // x is a variable, 2 is a literal.
+
+	int y = (x > 0) ? 1 : 2;
+
+	cout << y << endl;
+
+	return 0;
+}
