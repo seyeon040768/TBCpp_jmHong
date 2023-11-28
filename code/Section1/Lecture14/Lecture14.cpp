be402251e019efab44e982b456f5422f14f02146
Seyeon Lee <seyeon040768@gmail.com>
2023-11-28 23:47:39 +0900

    update(code): Section1 Lecture14

---
+++ code/Section1/Lecture14/Lecture14.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+#define MY_NUMBER "Hello, World"
+#define MAX(a, b) ((a > b) ? a : b)
+
+#define LIKE_APPLE
+
+int main(void)
+{
+	cout << MY_NUMBER << endl;
+	cout << MAX(1, 2) << endl;
+
+#ifdef LIKE_APPLE
+	cout << "Apple" << endl;
+#endif
+
+#ifndef LIKE_APPLE
+	cout << "Orange" << endl;
+#endif
+
+	return 0;
+}
