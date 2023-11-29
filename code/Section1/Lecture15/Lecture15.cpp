aa8330b833e60ff0f04b7ea7bb9ecbebc4ab5e4b
Seyeon Lee <seyeon040768@gmail.com>
2023-11-29 23:58:18 +0900

    edit(code): Section1 Lecture15

--- code/Section1/Lecture15/Lecture15.cpp
+++ code/Section1/Lecture15/Lecture15.cpp
@@ -6,11 +6,18 @@ int main(void)
 
 	bool bValue = false;
 	char chValue = 65;
-	float fValue = 3.141592;
+	float fValue = 3.141592f;
 	double dValue = 3.141592;
 
 	cout << fValue << endl;
 	cout << dValue << endl;
+
+
+	auto aValue = 3.141592;
+	auto aValue2 = 3.141592f;
+
+	cout << sizeof(aValue) << endl;
+	cout << sizeof(aValue2) << endl;
 
 	return 0;
 }
