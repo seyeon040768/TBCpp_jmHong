03ebbb77bcbcc358f7be4dbe08d5ff2d53b90b34
Seyeon Lee <seyeon040768@gmail.com>
2023-11-29 23:56:19 +0900

    update(code): Section1 Lecture15

---
+++ code/Section1/Lecture15/Lecture15.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void)
+{
+	using namespace std;
+
+	bool bValue = false;
+	char chValue = 65;
+	float fValue = 3.141592;
+	double dValue = 3.141592;
+
+	cout << fValue << endl;
+	cout << dValue << endl;
+
+	return 0;
+}
