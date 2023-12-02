cee6462202f73f552a56abb3fe93d6c2b096ebf7
Seyeon Lee <seyeon040768@gmail.com>
2023-12-02 23:29:00 +0900

    update(code): Section2 Lecture8

---
+++ code/Section2/Lecture8/Lecture8.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	float pi = 3.14f;
+	unsigned int u = 5u;
+	long l = 5l;
+
+	
+	int x = 0b1010;
+	cout << x << endl;
+
+	x = 012;
+	cout << x << endl;
+
+	x = 0xA;
+	cout << x << endl;
+
+	return 0;
+}
