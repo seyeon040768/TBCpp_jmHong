eb7fbfb8a5e5ba649b986bf751b71e35de82ea11
Seyeon Lee <seyeon040768@gmail.com>
2023-12-03 18:25:30 +0900

    update(code): Section3 Lecture3

---
+++ code/Section3/Lecture3/Lecture3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int x = 10, y = 10;
+	cout << x << " " << y << endl;
+	cout << ++x << " " << y++ << endl;
+	cout << x << " " << y << endl;
+
+	return 0;
+}
