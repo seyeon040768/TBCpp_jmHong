20d3c85ebaeac0d66556dfc3c22547b7ad48e633
Seyeon Lee <seyeon040768@gmail.com>
2023-12-16 03:07:07 +0900

    update(code): Section4 Lecture9

---
+++ code/Section4/Lecture9/Lecture9.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+	typedef double distance_t;
+
+	double distance1;
+	distance_t distance2;
+
+
+	vector<pair<string, int>> pairList1;
+
+	typedef vector<pair<string, int>> pairList_t;
+	// using pairList_t = vector<pair<string, int>>;
+	pairList_t pairList2;
+
+	return 0;
+}
