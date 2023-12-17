0d55c3582115c223483cd61317df3d84d7faf08d
seyeon040768 <seyeon040768@gmail.com>
2023-12-17 23:38:50 +0900

    update(code): Section5 Lecture1

---
+++ code/Section5/Lecture1/Lecture1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "Hello, World!" << endl;
+
+	// exit(0);
+	return 0;
+
+	/*cout << "Hello, World!" << endl;
+
+	return 0;*/
+}
