5b3413e63fe02f4eae8b9b5f23b3e8d8d0f2aa84
seyeon040768 <seyeon040768@gmail.com>
2023-12-23 22:47:30 +0900

    update(code): Section5 Lecture4

---
+++ code/Section5/Lecture4/Lecture4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	double x;
+
+	while (true)
+	{
+		for (int i = 0; i < 100; ++i)
+		{
+			if (i == 50)
+			{
+				goto breakLoop;
+			}
+			cout << i << " ";
+		}
+	}
+breakLoop:
+
+	return 0;
+}
