32ee3d2d688b41dce477c931d5ebe1409f748921
seyeon040768 <seyeon040768@gmail.com>
2023-12-23 23:16:44 +0900

    update(code): Section5 Lecture5

---
+++ code/Section5/Lecture5/Lecture5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int count = 0;
+
+	while (true)
+	{
+		cout << count << endl;
+		++count;
+
+		if (count == 10)
+		{
+			break;
+		}
+	}
+
+	return 0;
+}
