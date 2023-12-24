5d8a7954aa27ec8f0be84bf196ad9b86d90a0076
seyeon040768 <seyeon040768@gmail.com>
2023-12-24 22:48:06 +0900

    update(code): Section5 Lecture6

---
+++ code/Section5/Lecture6/Lecture6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int selection;
+
+	do
+	{
+		cout << "1. add" << endl;
+		cout << "2. sub" << endl;
+		cout << "3. mult" << endl;
+		cout << "4. div" << endl;
+		cin >> selection;
+	} while (selection <= 0 || selection >= 5);
+
+	cout << "You selected " << selection << endl;
+
+	return 0;
+}
