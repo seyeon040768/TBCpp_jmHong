1dbc408b2feea2bcded83b1279507a8d9bfe700d
seyeon040768 <seyeon040768@gmail.com>
2023-12-24 23:15:02 +0900

    update(code): Section5 Lecture7

---
+++ code/Section5/Lecture7/Lecture7.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Pow(int base, int exponent)
+{
+	int result = 1;
+
+	for (int count = 0; count < exponent; ++count)
+	{
+		result *= base;
+	}
+
+	return result;
+}
+
+int main(void)
+{
+	for (int count = 0; count < 10; ++count)
+	{
+		cout << count << endl;
+	}
+
+	cout << Pow(2, 10) << endl;
+
+	for (int i = 0, j = 0; i < 5; ++i, j += 2)
+	{
+		cout << i << " " << j << endl;
+	}
+
+	return 0;
+}
