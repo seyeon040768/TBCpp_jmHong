e7a05958e35daeb81a8e71150b26df2a5e5ade26
seyeon040768 <seyeon040768@gmail.com>
2023-12-25 01:15:39 +0900

    update(code): Section5 Lecture8

---
+++ code/Section5/Lecture8/Lecture8.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int count = 0;
+
+	while (true)
+	{
+		cout << count << endl;
+
+		if (count == 10)
+		{
+			break;
+		}
+
+		++count;
+	}
+
+	while (true)
+	{
+		char ch;
+		cin >> ch;
+
+		cout << ch << endl;
+
+		if (ch == 'q')
+		{
+			break;
+		}
+	}
+
+
+	for (int i = 0; i < 10; ++i)
+	{
+		if (i % 2 == 0)
+		{
+			continue;
+		}
+
+		cout << i << endl;
+	}
+
+	return 0;
+}
