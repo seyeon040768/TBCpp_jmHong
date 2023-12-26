55f01b988ba21a4115974df92a43540f2af9bc6a
seyeon040768 <seyeon040768@gmail.com>
2023-12-26 23:20:30 +0900

    update(code): Section6 Lecture3

---
+++ code/Section6/Lecture3/Lecture3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int scores[] = { 84, 92, 76, 81, 56 };
+
+	const int numStudents = sizeof(scores) / sizeof(int);
+
+	int totalScore = 0;
+
+	for (int i = 0; i < numStudents; ++i)
+	{
+		totalScore += scores[i];
+	}
+
+
+	int maxScore = 0;
+
+	for (int i = 0; i < numStudents; ++i)
+	{
+		if (scores[i] > maxScore)
+		{
+			maxScore = scores[i];
+		}
+	}
+
+	return 0;
+}
