7bd0e5f053c564b06d832bca99ed4685cb7d1bbf
seyeon040768 <seyeon040768@gmail.com>
2023-12-26 23:50:44 +0900

    update(code): Section6 Lecture5

---
+++ code/Section6/Lecture5/Lecture5.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	const int numRows = 3;
+	const int numColumns = 5;
+	int arr[numRows][numColumns] = // row-major
+	{
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+		{11, 12, 13, 14, 15}
+	};
+
+	for (int row = 0; row < numRows; ++row)
+	{
+		for (int col = 0; col < numColumns; ++col)
+		{
+			cout << arr[row][col] << "\t";
+		}
+
+		cout << endl;
+	}
+
+	return 0;
+}
