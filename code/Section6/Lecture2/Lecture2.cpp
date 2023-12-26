3cef4b993d83cc44cea04cde5d01b77a826b42a2
seyeon040768 <seyeon040768@gmail.com>
2023-12-26 22:56:49 +0900

    update(code): Section6 Lecture2

---
+++ code/Section6/Lecture2/Lecture2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void DoSomething(int studentsScores[20]) // translated into int*
+{
+	cout << &studentsScores << endl; // 000000D858F2F700
+	cout << &studentsScores[0] << endl; // 000000D858F2F750
+	cout << &studentsScores[1] << endl; // 000000D858F2F754
+	cout << &studentsScores[2] << endl; // 000000D858F2F758
+
+	cout << sizeof(studentsScores) << endl; // 8
+}
+
+int main(void)
+{
+	const int numStudents = 20;
+
+	int studentsScores[numStudents];
+
+	cout << &studentsScores << endl; // 000000D858F2F750
+	cout << &studentsScores[0] << endl; // 000000D858F2F750
+	cout << &studentsScores[1] << endl; // 000000D858F2F754
+	cout << &studentsScores[2] << endl; // 000000D858F2F758
+
+	cout << sizeof(studentsScores) << endl; // 80
+
+	DoSomething(studentsScores);
+
+	return 0;
+}
